f52fa11caeefeff75eaf767d8d5739d0b77a9cd7
CookieBorn <robanikin@hotmail.com>
2025-04-06 08:50:56 +1000

    B: Server Function

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/CookieBorn/chirpy
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	servMux := http.NewServeMux()
+	servStruct := http.Server{
+		Addr:    ":8081",
+		Handler: servMux,
+	}
+	err := servStruct.ListenAndServe()
+	if err != nil {
+		fmt.Printf("%v", err)
+	}
+}
