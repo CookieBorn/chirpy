d1cc5b09df11c49a710d16a7f88a9f0c400fc3a7
CookieBorn <robanikin@hotmail.com>
2025-04-08 21:24:13 +1000

    F: Create User

---
+++ internal/helpers/structs.go
@@ -0,0 +1,92 @@
+package healpers
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/CookieBorn/chirpy/internal/database"
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+)
+
+type ReturnErr struct {
+	Err string `json:"error"`
+}
+
+type ValidRet struct {
+	Valid        bool   `json:"valid"`
+	Cleaned_body string `json:"cleaned_body"`
+}
+
+type User struct {
+	Id         uuid.UUID `json:"id"`
+	Created_at time.Time `json:"created_at"`
+	Updated_at time.Time `json:"updated_at"`
+	Email      string    `json:"email"`
+}
+
+func StringCleaner(s string) string {
+	slice := strings.Split(s, " ")
+	for int, sli := range slice {
+		if strings.ToLower(sli) == "kerfuffle" || strings.ToLower(sli) == "sharbert" || strings.ToLower(sli) == "fornax" {
+			slice[int] = "****"
+		}
+	}
+	string := strings.Join(slice, " ")
+	return string
+}
+
+func DatabaseConnection() *database.Queries {
+	godotenv.Load(".env")
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		fmt.Printf("Open connection error: %v", err)
+	}
+	dbQueries := database.New(db)
+	return dbQueries
+}
+
+func RespondWithError(w http.ResponseWriter, code int, msg string) {
+	errRet := ReturnErr{
+		Err: msg,
+	}
+	w.WriteHeader(code)
+	dat, err := json.Marshal(errRet)
+	if err != nil {
+		fmt.Printf("Marshal error: %v", err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(dat)
+	return
+}
+
+func RespondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	tru, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(400)
+		return
+	}
+	w.WriteHeader(code)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(tru)
+}
+
+func DecoderHealper(res http.ResponseWriter, req *http.Request, parameters any) any {
+	decoder := json.NewDecoder(req.Body)
+	params := parameters
+	err := decoder.Decode(&params)
+	if err != nil {
+		fmt.Printf("Decoding error: %v", err)
+		res.WriteHeader(400)
+		return parameters
+	}
+	return params
+}
--- main.go
+++ main.go
@@ -4,18 +4,30 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/joho/godotenv"
+
+	"github.com/CookieBorn/chirpy/internal/database"
+	healpers "github.com/CookieBorn/chirpy/internal/helpers"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	apiC := apiConfig{}
-	apiC.fileserverHits.Store(0)
+	dbQueries := healpers.DatabaseConnection()
+	apiC := ApiConfig{
+		DB: dbQueries,
+	}
+	apiC.FileserverHits.Store(0)
 	servMux := http.NewServeMux()
 	servMux.Handle("/app/", apiC.middlewareMetricsInc(http.FileServer(http.Dir("."))))
 	servMux.HandleFunc("GET /api/healthz", ReadinessHandeler)
 	servMux.HandleFunc("GET /admin/metrics", apiC.metricHandle)
 	servMux.HandleFunc("POST /admin/reset", apiC.metricReset)
 	servMux.HandleFunc("POST /api/validate_chirp", postHandle)
+	servMux.HandleFunc("POST /api/users", apiC.createUserHandle)
 	http.StripPrefix("app/", servMux)
 	servStruct := http.Server{
 		Addr:    ":8081",
@@ -37,32 +49,39 @@ func ReadinessHandeler(res http.ResponseWriter, req *http.Request) {
 	}
 }
 
-type apiConfig struct {
-	fileserverHits atomic.Int32
+type ApiConfig struct {
+	FileserverHits atomic.Int32
+	DB             *database.Queries
 }
 
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+func (cfg *ApiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
+		cfg.FileserverHits.Add(1)
 		next.ServeHTTP(w, r)
 	})
 }
 
-func (cfg *apiConfig) metricHandle(res http.ResponseWriter, req *http.Request) {
+func (cfg *ApiConfig) metricHandle(res http.ResponseWriter, req *http.Request) {
 	req.Header.Set("Content-Type", "text/html")
 	res.WriteHeader(200)
-	write := []byte(fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", cfg.fileserverHits.Load()))
+	write := []byte(fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", cfg.FileserverHits.Load()))
 	int, err := res.Write(write)
 	if err != nil {
 		fmt.Printf("%d: %v", int, err)
 	}
 }
 
-func (cfg *apiConfig) metricReset(res http.ResponseWriter, req *http.Request) {
-	cfg.fileserverHits.Add(-cfg.fileserverHits.Load())
+func (cfg *ApiConfig) metricReset(res http.ResponseWriter, req *http.Request) {
+	godotenv.Load(".env")
+	dev := os.Getenv("PLATFORM")
+	if dev != "dev" {
+		healpers.RespondWithError(res, 403, "Forbidden")
+	}
+	cfg.DB.Reset(req.Context())
+	cfg.FileserverHits.Add(-cfg.FileserverHits.Load())
 	req.Header.Set("Content-Type", "text/plain")
 	res.WriteHeader(200)
-	write := []byte(fmt.Sprintf("Reset Successful hits: %v", cfg.fileserverHits.Load()))
+	write := []byte(fmt.Sprintf("Reset Successful hits: %v\n Users deleted", cfg.FileserverHits.Load()))
 	int, err := res.Write(write)
 	if err != nil {
 		fmt.Printf("%d: %v", int, err)
@@ -82,31 +101,35 @@ func postHandle(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 	if len([]rune(params.Body)) > 140 {
-		errRet := returnErr{
-			Err: "Chirp is too long",
-		}
-		res.WriteHeader(400)
-		dat, err := json.Marshal(errRet)
-		if err != nil {
-			fmt.Printf("Marshal error: %v", err)
-			return
-		}
-		res.Header().Set("Content-Type", "application/json")
-		res.Write(dat)
-		return
+		healpers.RespondWithError(res, 400, "Chirpy is too long")
 	}
-	clean := stringCleaner(params.Body)
-	valid := validRet{
+	clean := healpers.StringCleaner(params.Body)
+	valid := healpers.ValidRet{
 		Valid:        true,
 		Cleaned_body: clean,
 	}
-	tru, err := json.Marshal(valid)
+	healpers.RespondWithJSON(res, 200, valid)
+}
+
+func (cfg *ApiConfig) createUserHandle(res http.ResponseWriter, req *http.Request) {
+	type parameters struct {
+		Email string `json:"email"`
+	}
+	decoder := json.NewDecoder(req.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
 	if err != nil {
-		fmt.Printf("Error marshalling JSON: %s", err)
-		res.WriteHeader(500)
+		fmt.Printf("Decoding error: %v", err)
+		res.WriteHeader(400)
 		return
 	}
-	res.WriteHeader(200)
-	res.Header().Set("Content-Type", "application/json")
-	res.Write(tru)
+
+	usr, err := cfg.DB.CreateUser(req.Context(), params.Email)
+	UserStruct := healpers.User{
+		Id:         usr.ID,
+		Created_at: usr.CreatedAt,
+		Updated_at: usr.UpdatedAt,
+		Email:      usr.Email,
+	}
+	healpers.RespondWithJSON(res, 201, UserStruct)
 }
